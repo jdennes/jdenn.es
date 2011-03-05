1921ebd2500c46b3270afd1416c7406589734d0d
James Dennes <jdennes@gmail.com>
2011-03-05 21:53:58 +1100

    Layout and styles for coding data.

--- public/js/github.js
+++ public/js/github.js
@@ -5,13 +5,15 @@
     },
     settings = $.extend({}, defaults, options),
     url = 'http://pipes.yahoo.com/pipes/pipe.run?_id=a6bc41631b551cf342113be8c733a329&_render=json&limit=' + settings.count + '&_callback=?',
-    displayElement = $(this);
+    displayElement = $(this),
+    grav = "https://secure.gravatar.com/avatar/55fd031da91ef9af6e6ed88b101416a1?s=34&d=https://github.com/images/gravatars/gravatar-140.png";
     displayElement.empty();
     $.getJSON(url, function(data) {
       var html = [];
       html.push('<ul>');
       $.each(data.value.items, function(i, item) {
         html.push('<li>');
+        html.push('<div class="grav"><img src="' + grav + '" width="34" height="34" /></div>');
         // Add 8 hours to utime, as it's not in GMT
         html.push('<a href="' + item.link + '">' + prettyDate(parseInt(item["y:published"].utime) + 28800) + ' ' + 
           item.title + '</a><div class="item-details">' +item.content.content + '</div>');
